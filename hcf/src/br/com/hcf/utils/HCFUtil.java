6399ddb02ec0178be6747ee732db5ce1a63dfdfc
Eduardo William Karpinski Priester <45966684+Eduardo-Karpinski@users.noreply.github.com>
2021-04-06 23:18:56 -0300

    Atualizando HCFUtil

--- hcf/src/br/com/hcf/utils/HCFUtil.java
+++ hcf/src/br/com/hcf/utils/HCFUtil.java
@@ -2,13 +2,15 @@ package br.com.hcf.utils;
 
 import java.io.File;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -29,14 +31,44 @@ public final class HCFUtil {
 	}
 	
 	public static void insertAnnotatedClasses() {
+		HCFUtil util = new HCFUtil();
 		Set<Class<?>> classesWithAnnotations = new HashSet<>();
 		Arrays.asList(Package.getPackages()).stream()
-			.filter(p -> !p.getName().startsWith("sun") && !p.getName().startsWith("java") && !p.getName().startsWith("jdk"))
-			.forEach(p -> classesWithAnnotations.addAll(findClasses(p.getName())));
+			.filter(util.checkPackages())
+			.forEach(p -> classesWithAnnotations.addAll(util.findClasses(p.getName())));
 		HCFactory.setAnnotatedClasses(classesWithAnnotations);
 	}
+
+	private Predicate<? super Package> checkPackages() {
+		return p -> !p.getName().startsWith("sun") &&
+					!p.getName().startsWith("java") &&
+					!p.getName().startsWith("jdk") &&
+					!p.getName().startsWith("javax") &&
+					!p.getName().startsWith("jakarta") &&
+					!p.getName().startsWith("br.com.hcf") &&
+					!p.getName().startsWith("com.mysql") &&
+					!p.getName().startsWith("com.sun") &&
+					!p.getName().startsWith("io.smallrye") &&
+					!p.getName().startsWith("io.undertow") &&
+					!p.getName().startsWith("io.jaegertracing") &&
+					!p.getName().startsWith("org.wildfly") &&
+					!p.getName().startsWith("org.jboss") &&
+					!p.getName().startsWith("org.jberet") &&
+					!p.getName().startsWith("org.glassfish") &&
+					!p.getName().startsWith("org.hibernate") &&
+					!p.getName().startsWith("org.slf4j") &&
+					!p.getName().startsWith("org.eclipse") &&
+					!p.getName().startsWith("org.picketbox") &&
+					!p.getName().startsWith("org.slf4j") &&
+					!p.getName().startsWith("org.jcp") &&
+					!p.getName().startsWith("org.ietf") &&
+					!p.getName().startsWith("org.xml") &&
+					!p.getName().startsWith("org.w3c") &&
+					!p.getName().startsWith("org.graalvm") &&
+					!p.getName().startsWith("org.apache");
+	}
 	
-	private static Set<Class<?>> findClasses(String packageName) {
+	private Set<Class<?>> findClasses(String packageName) {
 		try {
 			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 			String path = packageName.replace('.', '/');
@@ -57,28 +89,57 @@ public final class HCFUtil {
 		}
 	}
 
-	private static Set<Class<?>> getClasses(File directory, String packageName) {
+	private Set<Class<?>> getClasses(File directory, String packageName) {
 		try {
 			Set<Class<?>> classes = new HashSet<Class<?>>();
-			if (!directory.exists()) {
+			
+			if (directory.getPath().startsWith("file:")) {
+				getClassesFromZip(directory.getPath().substring(0, directory.getPath().indexOf("!")), packageName, classes);
+				return classes;
+            } else if (!directory.exists()) {
+				return classes;
+			} else {
+				File[] files = directory.listFiles();
+				for (File file : files) {
+					if (file.isDirectory()) {
+						classes.addAll(getClasses(file, packageName + "." + file.getName()));
+					} else if (file.getName().endsWith(".class")) {
+						classes.add(Class.forName(packageName + '.' + file.getName().replaceAll("[.]class", "")));
+					}
+				}
 				return classes;
 			}
-			File[] files = directory.listFiles();
-			for (File file : files) {
-				if (file.isDirectory()) {
-					classes.addAll(getClasses(file, packageName + "." + file.getName()));
-				} else if (file.getName().endsWith(".class")) {
-					classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-				}
-			}
-			return classes;
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new HashSet<Class<?>>();
 		}
 	}
+
+	private void getClassesFromZip(String path, String packageName, Set<Class<?>> classes) {
+		try (ZipInputStream zip = new ZipInputStream(new URL(path).openStream())) {
+			ZipEntry entry;
+			while ((entry = zip.getNextEntry()) != null) {
+				if (entry.getName().endsWith(".class")) {
+					String className = entry.getName()
+							.replaceAll("[$].*", "")
+							.replaceAll("[.]class", "")
+							.replace('/', '.');
+					if (className.startsWith(packageName)) {
+						try {
+							classes.add(Class.forName(className));
+						} catch (Exception e) {
+							System.err.println("ERROR " + packageName+"."+className + " is unreadable.");
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
-	private static Set<Class<?>> getClassesWithAnnotation(Set<Class<?>> classes, Class<? extends Annotation> annotation) {
+	private Set<Class<?>> getClassesWithAnnotation(Set<Class<?>> classes, Class<? extends Annotation> annotation) {
 		Set<Class<?>> withAnnotation = new HashSet<Class<?>>();
 		classes.forEach(c -> {
 			if (Arrays.asList(c.getAnnotations()).stream().anyMatch(anotacao -> anotacao.annotationType().equals(annotation))) {
