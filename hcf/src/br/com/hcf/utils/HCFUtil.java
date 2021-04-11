8c2c4653713334611e2626452a60eb2e769d8159
Eduardo William Karpinski Priester <45966684+Eduardo-Karpinski@users.noreply.github.com>
2021-04-11 01:03:38 -0300

    Refatorando HCFUtil

--- hcf/src/br/com/hcf/utils/HCFUtil.java
+++ hcf/src/br/com/hcf/utils/HCFUtil.java
@@ -1,152 +1,66 @@
 package br.com.hcf.utils;
 
-import java.io.File;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.net.URL;
 import java.util.Arrays;
-import java.util.Enumeration;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import org.reflections.Reflections;
+
 import br.com.hcf.HCFactory;
 
 public final class HCFUtil {
-	
+
 	private HCFUtil() {
-		
+
 	}
-	
+
 	public static <T> String getId(Class<T> classe) {
 		return Arrays.asList(classe.getDeclaredFields()).stream()
-		.filter(f -> Arrays.asList(f.getAnnotations()).stream().anyMatch(a -> a instanceof Id))
-		.map(Field::getName)
-		.findFirst().orElseThrow(() -> new NullPointerException("Undeclared id"));
+				.filter(f -> f.getAnnotation(Id.class) != null)
+				.map(Field::getName)
+				.findFirst().orElseThrow(() -> new NullPointerException("Undeclared id"));
 	}
-	
+
 	public static void insertAnnotatedClasses() {
-		HCFUtil util = new HCFUtil();
-		Set<Class<?>> classesWithAnnotations = new HashSet<>();
-		Arrays.asList(Package.getPackages()).stream()
-			.filter(util.checkPackages())
-			.forEach(p -> classesWithAnnotations.addAll(util.findClasses(p.getName())));
-		HCFactory.setAnnotatedClasses(classesWithAnnotations);
+		HCFactory.setAnnotatedClasses(Set.of(Package.getPackages()).stream()
+				.filter(new HCFUtil().checkPackages())
+				.map(Package::getName).map(name -> new Reflections(name))
+				.map(HCFPackageValidator.classValid(reflections -> reflections.getTypesAnnotatedWith(Entity.class)))
+				.flatMap(Collection::stream)
+				.collect(Collectors.toSet()));
 	}
-
+	
+	/**
+	 * org.jboss.security.acl.ACLImpl is an entity
+	 * org.jboss.security.acl.ACLEntryImpl is an entity
+	 */
 	private Predicate<? super Package> checkPackages() {
-		return p -> !p.getName().startsWith("sun") &&
-					!p.getName().startsWith("java") &&
-					!p.getName().startsWith("jdk") &&
-					!p.getName().startsWith("javax") &&
-					!p.getName().startsWith("jakarta") &&
-					!p.getName().startsWith("br.com.hcf") &&
-					!p.getName().startsWith("com.mysql") &&
-					!p.getName().startsWith("com.sun") &&
-					!p.getName().startsWith("io.smallrye") &&
-					!p.getName().startsWith("io.undertow") &&
-					!p.getName().startsWith("io.jaegertracing") &&
-					!p.getName().startsWith("org.wildfly") &&
-					!p.getName().startsWith("org.jboss") &&
-					!p.getName().startsWith("org.jberet") &&
-					!p.getName().startsWith("org.glassfish") &&
-					!p.getName().startsWith("org.hibernate") &&
-					!p.getName().startsWith("org.slf4j") &&
-					!p.getName().startsWith("org.eclipse") &&
-					!p.getName().startsWith("org.picketbox") &&
-					!p.getName().startsWith("org.slf4j") &&
-					!p.getName().startsWith("org.jcp") &&
-					!p.getName().startsWith("org.ietf") &&
-					!p.getName().startsWith("org.xml") &&
-					!p.getName().startsWith("org.w3c") &&
-					!p.getName().startsWith("org.graalvm") &&
-					!p.getName().startsWith("org.apache");
-	}
-	
-	private Set<Class<?>> findClasses(String packageName) {
-		try {
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-			String path = packageName.replace('.', '/');
-			Enumeration<URL> resources = classLoader.getResources(path);
-			Set<File> dirs = new HashSet<File>();
-			while (resources.hasMoreElements()) {
-				URL resource = resources.nextElement();
-				dirs.add(new File(resource.getFile()));
-			}
-			Set<Class<?>> classes = new HashSet<Class<?>>();
-			for (File directory : dirs) {
-				classes.addAll(getClasses(directory, packageName));
-			}
-			return getClassesWithAnnotation(classes, Entity.class);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new HashSet<Class<?>>();
-		}
+		return p -> !p.getName().startsWith("org.jboss");
 	}
 
-	private Set<Class<?>> getClasses(File directory, String packageName) {
-		try {
-			Set<Class<?>> classes = new HashSet<Class<?>>();
-			
-			if (directory.getPath().startsWith("file:")) {
-				getClassesFromZip(directory.getPath().substring(0, directory.getPath().indexOf("!")), packageName, classes);
-				return classes;
-            } else if (!directory.exists()) {
-				return classes;
-			} else {
-				File[] files = directory.listFiles();
-				for (File file : files) {
-					if (file.isDirectory()) {
-						classes.addAll(getClasses(file, packageName + "." + file.getName()));
-					} else if (file.getName().endsWith(".class")) {
-						classes.add(Class.forName(packageName + '.' + file.getName().replaceAll("[.]class", "")));
-					}
-				}
-				return classes;
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new HashSet<Class<?>>();
-		}
-	}
+}
 
-	private void getClassesFromZip(String path, String packageName, Set<Class<?>> classes) {
-		try (ZipInputStream zip = new ZipInputStream(new URL(path).openStream())) {
-			ZipEntry entry;
-			while ((entry = zip.getNextEntry()) != null) {
-				if (entry.getName().endsWith(".class")) {
-					String className = entry.getName()
-							.replaceAll("[$].*", "")
-							.replaceAll("[.]class", "")
-							.replace('/', '.');
-					if (className.startsWith(packageName)) {
-						try {
-							classes.add(Class.forName(className));
-						} catch (Exception e) {
-							System.err.println("ERROR " + packageName+"."+className + " is unreadable.");
-						}
-					}
-				}
+@FunctionalInterface 
+interface HCFPackageValidator<I, O, T extends Throwable> {
+
+	Set<Class<?>> apply(I t) throws T;
+
+	static <I, O, T extends Throwable> Function<I, Set<Class<?>>> classValid(HCFPackageValidator<I, Set<Class<?>>, T> validator) {
+		return c -> {
+			try {
+				return validator.apply(c);
+			} catch (Throwable e) {
+				return new HashSet<Class<?>>();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private Set<Class<?>> getClassesWithAnnotation(Set<Class<?>> classes, Class<? extends Annotation> annotation) {
-		Set<Class<?>> withAnnotation = new HashSet<Class<?>>();
-		classes.forEach(c -> {
-			if (Arrays.asList(c.getAnnotations()).stream().anyMatch(anotacao -> anotacao.annotationType().equals(annotation))) {
-				withAnnotation.add(c);
-			}
-		});
-		return withAnnotation;
+		};
 	}
 
 }
