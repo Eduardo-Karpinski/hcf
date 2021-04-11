6f6f974e1616e27e553bb066a8267e286f1fc8c7
Eduardo William Karpinski Priester <45966684+Eduardo-Karpinski@users.noreply.github.com>
2021-04-11 01:07:41 -0300

    Adicionando requires

--- hcf/src/module-info.java
+++ hcf/src/module-info.java
@@ -1,8 +1,10 @@
+@SuppressWarnings("module")
 module hibernateConnectorFacilitator {
 	exports br.com.hcf;
 	exports br.com.hcf.enums;
 	exports br.com.hcf.annotations;
 	
+	requires reflections; // Name of automatic module 'reflections' is unstable
 	requires java.naming;
 	requires java.persistence;
 	
