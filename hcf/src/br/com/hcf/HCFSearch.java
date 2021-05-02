6518bd5203721170fa220fb4b5774360f64a7a2c
Eduardo William Karpinski Priester <45966684+Eduardo-Karpinski@users.noreply.github.com>
2021-05-02 00:24:06 -0300

    Adicionando hashCode, equals e toString

--- hcf/src/br/com/hcf/HCFSearch.java
+++ hcf/src/br/com/hcf/HCFSearch.java
@@ -49,4 +49,47 @@ public class HCFSearch {
 		return this;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((field == null) ? 0 : field.hashCode());
+		result = prime * result + ((operator == null) ? 0 : operator.hashCode());
+		result = prime * result + ((parameter == null) ? 0 : parameter.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HCFSearch other = (HCFSearch) obj;
+		if (field == null) {
+			if (other.field != null)
+				return false;
+		} else if (!field.equals(other.field))
+			return false;
+		if (operator != other.operator)
+			return false;
+		if (parameter != other.parameter)
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "HCFSearch [field=" + field + ", value=" + value + ", parameter=" + parameter + ", operator=" + operator
+				+ "]";
+	}
+
 }
