a62b916d0eb37a8e998c4b24c6f13c726340c98b
Eduardo William Karpinski Priester <45966684+Eduardo-Karpinski@users.noreply.github.com>
2021-05-02 00:23:32 -0300

    Adicionando hashCode, equals e toString

--- hcf/src/br/com/hcf/HCFOrder.java
+++ hcf/src/br/com/hcf/HCFOrder.java
@@ -18,32 +18,84 @@ public final class HCFOrder {
 		return asc;
 	}
 
-	public void setAsc(Boolean asc) {
+	public HCFOrder setAsc(Boolean asc) {
 		this.asc = asc;
+		return this;
 	}
 
 	public String getField() {
 		return field;
 	}
 
-	public void setField(String field) {
+	public HCFOrder setField(String field) {
 		this.field = field;
+		return this;
 	}
 
 	public Integer getLimit() {
 		return limit;
 	}
 
-	public void setLimit(Integer limit) {
+	public HCFOrder setLimit(Integer limit) {
 		this.limit = limit;
+		return this;
 	}
 
 	public Integer getOffset() {
 		return offset;
 	}
 
-	public void setOffset(Integer offset) {
+	public HCFOrder setOffset(Integer offset) {
 		this.offset = offset;
+		return this;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((asc == null) ? 0 : asc.hashCode());
+		result = prime * result + ((field == null) ? 0 : field.hashCode());
+		result = prime * result + ((limit == null) ? 0 : limit.hashCode());
+		result = prime * result + ((offset == null) ? 0 : offset.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HCFOrder other = (HCFOrder) obj;
+		if (asc == null) {
+			if (other.asc != null)
+				return false;
+		} else if (!asc.equals(other.asc))
+			return false;
+		if (field == null) {
+			if (other.field != null)
+				return false;
+		} else if (!field.equals(other.field))
+			return false;
+		if (limit == null) {
+			if (other.limit != null)
+				return false;
+		} else if (!limit.equals(other.limit))
+			return false;
+		if (offset == null) {
+			if (other.offset != null)
+				return false;
+		} else if (!offset.equals(other.offset))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "HCFOrder [asc=" + asc + ", field=" + field + ", limit=" + limit + ", offset=" + offset + "]";
 	}
 
 }
