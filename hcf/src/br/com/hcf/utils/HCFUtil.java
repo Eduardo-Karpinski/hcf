c516fe35493b5464d3927824b08a0f869d34fd49
Eduardo William Karpinski Priester <45966684+Eduardo-Karpinski@users.noreply.github.com>
2021-09-30 00:39:31 -0300

    Add files via upload

--- hcf/src/br/com/hcf/utils/HCFUtil.java
+++ hcf/src/br/com/hcf/utils/HCFUtil.java
@@ -20,16 +20,9 @@ public final class HCFUtil {
 	}
 
 	public static <T> String getId(Class<T> classe) {
-		
 		Reflections reflections = new Reflections(classe, new FieldAnnotationsScanner());
 		Set<Field> ids = reflections.getFieldsAnnotatedWith(Id.class);
-		
-		if (ids.isEmpty()) {
-			return getId(classe.getSuperclass());
-		} else {
-			return ids.iterator().next().getName();
-		}
-		
+		return ids.isEmpty() ? getId(classe.getSuperclass()) : ids.iterator().next().getName();
 	}
 
 	public static Set<Class<?>> getAnnotatedClasses() {
@@ -47,17 +40,35 @@ public final class HCFUtil {
 	
 	// i need to make it automatic
 	private static boolean checkPackageName(String name) {
-		return !name.startsWith("br.com.hcf") && !name.startsWith("ch.qos") && !name.startsWith("com.fasterxml")
-				&& !name.startsWith("com.sun") && !name.startsWith("com.mysql") && !name.startsWith("io.jaegertracing")
-				&& !name.startsWith("io.smallrye") && !name.startsWith("io.undertow") && !name.startsWith("java")
-				&& !name.startsWith("jakarta") && !name.startsWith("jdk") && !name.startsWith("net.bytebuddy")
-				&& !name.startsWith("org.apache") && !name.startsWith("org.eclipse")
-				&& !name.startsWith("org.glassfish") && !name.startsWith("org.graalvm")
-				&& !name.startsWith("org.hibernate") && !name.startsWith("org.ietf") && !name.startsWith("org.jberet")
-				&& !name.startsWith("org.jcp") && !name.startsWith("org.jboss") && !name.startsWith("org.reflections")
-				&& !name.startsWith("org.picketbox") && !name.startsWith("org.slf4j")
-				&& !name.startsWith("org.springframework") && !name.startsWith("org.wildfly")
-				&& !name.startsWith("org.w3c.dom") && !name.startsWith("org.xml") && !name.startsWith("org.yaml")
+		return !name.startsWith("br.com.hcf") 
+				&& !name.startsWith("ch.qos") 
+				&& !name.startsWith("com.fasterxml")
+				&& !name.startsWith("com.sun") 
+				&& !name.startsWith("com.mysql") 
+				&& !name.startsWith("io.jaegertracing")
+				&& !name.startsWith("io.smallrye") 
+				&& !name.startsWith("io.undertow") 
+				&& !name.startsWith("java")
+				&& !name.startsWith("jakarta") 
+				&& !name.startsWith("jdk")
+				&& !name.startsWith("net.bytebuddy")
+				&& !name.startsWith("org.apache") 
+				&& !name.startsWith("org.eclipse")
+				&& !name.startsWith("org.glassfish") 
+				&& !name.startsWith("org.graalvm")
+				&& !name.startsWith("org.hibernate") 
+				&& !name.startsWith("org.ietf") 
+				&& !name.startsWith("org.jberet")
+				&& !name.startsWith("org.jcp") 
+				&& !name.startsWith("org.jboss") 
+				&& !name.startsWith("org.reflections")
+				&& !name.startsWith("org.picketbox") 
+				&& !name.startsWith("org.slf4j")
+				&& !name.startsWith("org.springframework") 
+				&& !name.startsWith("org.wildfly")
+				&& !name.startsWith("org.w3c.dom") 
+				&& !name.startsWith("org.xml") 
+				&& !name.startsWith("org.yaml")
 				&& !name.startsWith("sun");
 	}
 
